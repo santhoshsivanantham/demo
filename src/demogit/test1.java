12c6f60a579eab694fb4f3906ecafe8737338b89
santhosh <santhoshsivanantham1@gmail.com>
2025-03-08 13:16:39 +0530

    commit to Qabranch

--- src/demogit/test1.java
+++ src/demogit/test1.java
@@ -1,9 +1,22 @@
-package demogit;
+ package demogit;
+
+import java.util.Scanner;
 
 public class test1 {
 
 	public static void main(String[] args) {
-		System.out.println("santhosj");
+		Scanner sc = new Scanner (System.in);
+		System.out.println("Enter the pattern range");
+		int n=sc.nextInt();
+				
+		for (int i = n; i >= 1; i--) 
+		{  
+            for (int j = i; j <= n; j++) 
+            {
+                System.out.print(n+"  ");
+            }
+            System.out.println();
+	    }	
 	}
 
 }
